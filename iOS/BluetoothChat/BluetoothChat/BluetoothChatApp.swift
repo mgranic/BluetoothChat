ba5f1917ca1e39bea596bdf603bc459959dcbc02
mgranic <mgranic11@gmail.com>
2024-12-04 13:24:56 +0100

    iOS: create new project
    empty SwiftUI project

---
+++ iOS/BluetoothChat/BluetoothChat/BluetoothChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  BluetoothChatApp.swift
+//  BluetoothChat
+//
+//  Created by Mate Granic on 04.12.2024..
+//
+
+import SwiftUI
+
+@main
+struct BluetoothChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
